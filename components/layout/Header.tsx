6b7c61dab11c32d7bbbe5472d87abef9984b6084
moonctp <moonctp24@gmail.com>
2024-02-23 17:09:26 +0900

    header 퍼블 완료

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,10 +1,11 @@
 const Header = () => {
   return (
     <>
-      <div className="w-[1570px] h-[100px] flex	border-gray-400	">
-        <div className="flex-1 w-[350px] h-[100px] align-middle text-left">대시보드</div>
-        <div className="flex-2 w-[80px] h-[80px] right-[0px] text-xl text-center">계정</div>
+      <div className="w-[1570px] h-[100px] flex">
+        <div className="flex-1 w-[350px] h-[100px] grid place-items-center text-left">대시보드</div>
+        <div className="flex-2 w-[70px] h-[70px] right-[0px] grid place-items-center text-xl text-center border align-middle border-yellow-50 rounded-full 	bg-red-100	">계정</div>
       </div>
+      <hr className="w-[1570px] border-gray-400 border" />
     </>
   );
 };
