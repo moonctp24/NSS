fdfcf4315535b8c603d17245b9e432aee3a031b1
moonctp <moonctp24@gmail.com>
2024-02-23 16:46:34 +0900

    관리자설정 페이지 생성

---
+++ components/layout/admSttng/AdmSttngComp.tsx
@@ -0,0 +1,9 @@
+const AdmSttngComp = () => {
+  return (
+    <>
+      <div>관리자설정 페이지 제작중</div>
+    </>
+  );
+};
+
+export default AdmSttngComp;
---
+++ pages/admSttng/index.tsx
@@ -0,0 +1,7 @@
+import AdmSttngComp from "@/components/layout/admSttng/AdmSttngComp";
+
+const AdmSttngPage = () => {
+  return <AdmSttngComp />;
+};
+
+export default AdmSttngPage;
