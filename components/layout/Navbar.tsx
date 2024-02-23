64d1b4f2394fe07d251d195bbb1d64b39e88f3e2
moonctp <moonctp24@gmail.com>
2024-02-23 16:46:58 +0900

    각 페이지 네비게이션바 메뉴 연결

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -1,12 +1,28 @@
+import { useRouter } from "next/router";
+
 const Navbar = () => {
+  const router = useRouter();
+
+  const goMenu = (pageName: string) => {
+    router.push(`/${pageName}`);
+  };
+
   return (
     <>
       <div className="w-[350px] h-[1080px] bg-[#E6E6E6]">
         <p className="w-[350px] h-28 text-[40px] text-center text-[#00c1a6]">LOGO</p>
-        <button className="navBtn">대시보드</button>
-        <button className="navBtn">회원관리</button>
-        <button className="navBtn">의약품관리</button>
-        <button className="navBtn">관리자설정</button>
+        <button className="navBtn" onClick={() => goMenu("")}>
+          대시보드
+        </button>
+        <button className="navBtn" onClick={() => goMenu("userMng")}>
+          회원관리
+        </button>
+        <button className="navBtn" onClick={() => goMenu("medicineMng")}>
+          의약품관리
+        </button>
+        <button className="navBtn" onClick={() => goMenu("admSttng")}>
+          관리자설정
+        </button>
         <p className="absolute left-[130px] top-[984px] text-xl text-center text-[#020202]">
           <span className="text-xl text-center text-[#020202]">로그아웃</span>
         </p>
