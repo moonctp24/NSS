91149f9799e90fc26525e91b17a6d0ee0b0b3f70
moonctp <moonctp24@gmail.com>
2024-03-02 23:07:51 +0900

    header css 수정

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -3,7 +3,7 @@ const Header = () => {
     <>
       <div className="w-[1570px] h-[100px] flex">
         <div className="flex-1 w-[350px] h-[100px] grid place-items-center text-left">대시보드</div>
-        <div className="flex-2 w-[70px] h-[70px] right-[0px] grid place-items-center text-xl text-center border align-middle border-yellow-50 rounded-full 	bg-red-100	">계정</div>
+        <div className="flex-2 w-[70px] h-[70px] right-[0px] grid place-items-center text-xl text-center border border-yellow-50 rounded-full 	bg-red-100	">계정</div>
       </div>
       <hr className="w-[1570px] border-gray-400 border" />
     </>
