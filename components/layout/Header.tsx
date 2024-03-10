20091f8697e0c1d388e0b39dcea072c894a8ca6d
moonctp <moonctp24@gmail.com>
2024-03-10 12:29:25 +0900

    Layout modified

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,11 +1,11 @@
 const Header = () => {
   return (
     <>
-      <div className="w-[1570px] h-[100px] flex">
+      <div className="w-full h-[100px] flex">
         <div className="flex-1 w-[350px] h-[100px] grid place-items-center text-left">대시보드</div>
         <div className="flex-2 w-[70px] h-[70px] right-[0px] grid place-items-center text-xl text-center border border-yellow-50 rounded-full 	bg-red-100	">계정</div>
       </div>
-      <hr className="w-[1570px] border-gray-400 border" />
+      <hr className="w-4/5 border-gray-400 border" />
     </>
   );
 };
--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -9,8 +9,8 @@ const Navbar = () => {
 
   return (
     <>
-      <div className="w-[350px] h-[1080px] bg-[#E6E6E6]">
-        <p className="w-[350px] h-28 text-[40px] text-center text-[#00c1a6]">LOGO</p>
+      <div className="w-1/6 h-[1080px] bg-[#E6E6E6]">
+        <p className="w-full h-28 text-[40px] text-center text-[#00c1a6]">LOGO</p>
         <button className="navBtn" onClick={() => goMenu("")}>
           대시보드
         </button>
@@ -23,11 +23,9 @@ const Navbar = () => {
         <button className="navBtn" onClick={() => goMenu("acct/admSttng")}>
           관리자설정
         </button>
-        {/* <p className="absolute left-[130px] top-[984px] text-xl text-center text-[#020202]"> */}
         <button className="navLogoutBtn" onClick={() => goMenu("nauth/login")}>
           로그아웃
         </button>
-        {/* </p> */}
       </div>
     </>
   );
