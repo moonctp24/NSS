61c14117f88e684f4e05f8fe56304c9770736fe6
moonctp <moonctp24@gmail.com>
2024-03-10 12:28:47 +0900

    구글이미지 가져온 임시 hostname 등록

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    // 구글이미지 가져온 임시 hostname 등록
+    domains: ["cdn2.hubspot.net", "localhost"],
+  },
 };
 export default nextConfig;
