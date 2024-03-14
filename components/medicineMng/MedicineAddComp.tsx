1673a0466ef6400a617aaad587354bc60659d098
moonctp <moonctp24@gmail.com>
2024-03-14 21:54:26 +0900

    의약품 추가 테이블 그리기

--- components/medicineMng/MedicineAddComp.tsx
+++ components/medicineMng/MedicineAddComp.tsx
@@ -1,8 +1,16 @@
 import { useState } from "react";
 import MedicineDtlTable from "./MedicineDtlTable";
+import { useRouter } from "next/router";
 
 const MedicineAddComp = () => {
   const [isSaveClicked, setIsSaveClicked] = useState(false);
+  const [admCmmtM, setAdmCmmtM] = useState("");
+
+  const router = useRouter();
+
+  const goMcdnListPage = () => {
+    router.push("/medicineMng/mdcnList");
+  };
 
   const mDtlAddSave = () => {
     setIsSaveClicked(true);
@@ -10,7 +18,7 @@ const MedicineAddComp = () => {
 
   const getModifiedData = (modifiedData: any) => {
     if (isSaveClicked) {
-      //   modifiedData.admin_comment = admCmmtM;
+      modifiedData.admin_comment = admCmmtM;
       console.log(modifiedData);
 
       // 의약품 추가 통신
@@ -31,11 +39,20 @@ const MedicineAddComp = () => {
     <>
       <div className="mainComponent">
         <h1>의약품 추가</h1>
-        <button className="w-[100px] h-[40px] float-right" onClick={mDtlAddSave}>
-          저장
-        </button>
+        <div className="relative w-[200px] h-[40px] flex float-right">
+          <button className="h-[40px] float-right" onClick={mDtlAddSave}>
+            저장
+          </button>
+          <button className="h-[40px] btn_gray float-right" onClick={goMcdnListPage}>
+            목록
+          </button>
+        </div>
         <div className="m-auto	mt-20">
           <MedicineDtlTable mDtlInfo={null} isSaveClicked={isSaveClicked} getModifiedData={getModifiedData} />
+        </div>
+        <h1>관리자 메모</h1>
+        <div className="w-full h-[150px] rounded-md bg-[#d9d9d9] border-4 border-[#b6b6b6] p-5">
+          <textarea className="admCommentTextArea" value={admCmmtM} onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) => setAdmCmmtM(e.target.value)} />
         </div>
       </div>
     </>
--- components/medicineMng/MedicineDtlTable.tsx
+++ components/medicineMng/MedicineDtlTable.tsx
@@ -1,5 +1,5 @@
 import { dateFormat } from "@/constants/util/commUtil";
-import axios from "axios";
+// import axios from "axios";
 import Image from "next/image";
 import { useEffect, useState } from "react";
 
@@ -13,19 +13,19 @@ const MedicineDtlTable = (props: any) => {
   const [keepM, setKeepM] = useState("");
 
   useEffect(() => {
-    setMdcnNm(mDtl.medicine_name);
-    setCompanyNm(mDtl.company_name);
-    setEffct(mDtl.effect);
-    setSdEffct(mDtl.side_effect);
-    setCautionM(mDtl.caution);
-    setKeepM(mDtl.keep_method);
+    setMdcnNm(mDtl?.medicine_name);
+    setCompanyNm(mDtl?.company_name);
+    setEffct(mDtl?.effect);
+    setSdEffct(mDtl?.side_effect);
+    setCautionM(mDtl?.caution);
+    setKeepM(mDtl?.keep_method);
   }, [mDtl]);
 
   useEffect(() => {
     console.log("clicked!!" + props.isSaveClicked);
     if (props.isSaveClicked) {
       let modifiedData = {
-        item_seq: mDtl.item_seq,
+        item_seq: mDtl?.item_seq,
         medicine_name: mdcnNm,
         company_name: companyNm,
         description: "테스트",
@@ -61,7 +61,7 @@ const MedicineDtlTable = (props: any) => {
           <tr></tr>
         </thead>
         <tbody>
-          {!!mDtl ? (
+          {!!mDtl ? ( // 의약품 수정 테이블
             <>
               <tr>
                 <td>번호</td>
@@ -115,9 +115,60 @@ const MedicineDtlTable = (props: any) => {
               </tr>
             </>
           ) : (
-            <tr>
-              <td colSpan={4}>내용을 불러올 수 없습니다.</td>
-            </tr>
+            // <tr>
+            //   <td colSpan={4}>내용을 불러올 수 없습니다.</td>
+            // </tr>
+            // 의약품 추가 테이블
+            <>
+              <tr>
+                <td>번호</td>
+                <td className=""></td>
+                <td>등록일시</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>의약품명</td>
+                <td>
+                  <input type="text" value={mdcnNm} onChange={(e: React.ChangeEvent<HTMLInputElement>) => setMdcnNm(e.target.value)}></input>
+                </td>
+                <td>수정일시</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>제조사</td>
+                <td>
+                  <input type="text" value={companyNm} onChange={(e: React.ChangeEvent<HTMLInputElement>) => setCompanyNm(e.target.value)}></input>
+                </td>
+                <td>제조일자</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>약 종류</td>
+                <td></td>
+                <td>약 이미지</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>효능</td>
+                <td>
+                  <input type="text" value={effct} onChange={(e: React.ChangeEvent<HTMLInputElement>) => setEffct(e.target.value)}></input>
+                </td>
+                <td>부작용</td>
+                <td>
+                  <input type="text" value={sdEffct} onChange={(e: React.ChangeEvent<HTMLInputElement>) => setSdEffct(e.target.value)}></input>
+                </td>
+              </tr>
+              <tr>
+                <td>보관법</td>
+                <td>
+                  <input type="text" value={keepM} onChange={(e: React.ChangeEvent<HTMLInputElement>) => setKeepM(e.target.value)}></input>
+                </td>
+                <td>주의사항</td>
+                <td>
+                  <input type="text" value={cautionM} onChange={(e: React.ChangeEvent<HTMLInputElement>) => setCautionM(e.target.value)}></input>
+                </td>
+              </tr>
+            </>
           )}
         </tbody>
       </table>
