ad1766105332489e09cb9fe92a2d00668341da95
moonctp <moonctp24@gmail.com>
2024-04-07 15:32:04 +0900

    common setting

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,22 @@
+import type { AppProps } from "next/app";
+import { Provider } from "react-redux";
+import Head from "next/head";
+// import CommModalGroup from "@/components/popup/CommModalGroup";
 import Layout from "@/components/layout/Layout";
 import store from "@/store";
 import "@/styles/globals.css";
-import type { AppProps } from "next/app";
-import { Provider } from "react-redux";
+import "@/styles/popup.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <meta charSet="utf-8" />
+        <link rel="icon" href="/favicon.ico" />
+        <title>NSS</title>
+      </Head>
       <Provider store={store}>
+        {/* <CommModalGroup /> */}
         <Layout>
           <Component {...pageProps} />
         </Layout>
