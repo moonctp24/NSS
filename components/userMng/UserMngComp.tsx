c5fdfb27032e1fa1e60230c8ea9b72bc7d6b2aab
moonctp <moonctp24@gmail.com>
2024-04-07 20:59:00 +0900

    User Management Page Layout Updated

--- components/userMng/UserMngComp.tsx
+++ components/userMng/UserMngComp.tsx
@@ -1,64 +1,76 @@
+import { useAxios } from "@/constants/util/API_UTIL";
+import { codeToKor } from "@/constants/util/commUtil";
 import Image from "next/image";
 import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 const UserMngComp = () => {
   const [usrList, setUsrList] = useState<any>(null);
   const [domLoaded, setDomLoaded] = useState(false); // 테이블 데이터 로딩 상태
 
   const router = useRouter();
-
-  const codeToKor = (code: string) => {
-    if (code === "ACTIVE") {
-      return "일반";
-    } else if (code === "ADMIN") {
-      return "관리자";
-    } else if (code === "INACTIVE") {
-      return "비활성화";
-    } else if (code === "REMOVED") {
-      return "탈퇴";
-    } else if (code === "BANNED") {
-      return "정지";
-    } else {
-      return "잘못된 형식";
-    }
-  };
+  const { code, response, fetchData } = useAxios();
 
   useEffect(() => {
-    setUsrList([
-      {
-        user_id: "memId_00000000000001",
-        user_name: "김이름",
-        user_status: "ACTIVE",
-        user_image: "https://cdn2.hubspot.net/hubfs/53/image8-2.jpg",
-      },
-      {
-        user_id: "memId_00000000000002",
-        user_name: "김이름1",
-        user_status: "ADMIN",
-        user_image: "https://cdn2.hubspot.net/hubfs/53/image8-2.jpg",
-      },
-      {
-        user_id: "memId_00000000000003",
-        user_name: "김이름2",
-        user_status: "INACTIVE",
-        user_image: "https://cdn2.hubspot.net/hubfs/53/image8-2.jpg",
-      },
-      {
-        user_id: "memId_00000000000004",
-        user_name: "김이름3",
-        user_status: "REMOVED",
-        user_image: "https://cdn2.hubspot.net/hubfs/53/image8-2.jpg",
-      },
-      {
-        user_id: "memId_00000000000005",
-        user_name: "김이름4",
-        user_status: "BANNED",
-        user_image: "https://cdn2.hubspot.net/hubfs/53/image8-2.jpg",
-      },
-    ]);
-    setTrdServerList(["일반", "관리자", "정지", "비활성화", "탈퇴"]);
-    setDomLoaded(true);
+    getResult();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [code, response, router]);
+
+  const getResult = useCallback(() => {
+    // console.log(`code :: ${code} / response :: `, response);
+    if (response && code == "200") {
+      console.log(response);
+      const tmpObject = response;
+      const tmpList: { [s: string]: any } = [];
+      // object 형으로 받아와서 배열 형태로 변환해주기
+      let i = 0;
+      for (const [key, value] of Object.entries(tmpObject)) {
+        tmpList[i] = value;
+        i++;
+      }
+      setUsrList(
+        tmpList.length > 0
+          ? tmpList
+          : [
+              {
+                user_id: "memId_00000000000001",
+                user_name: "김이름",
+                user_status: "ACTIVE",
+                user_image: "https://cdn2.hubspot.net/hubfs/53/image8-2.jpg",
+              },
+              {
+                user_id: "memId_00000000000002",
+                user_name: "김이름1",
+                user_status: "ADMIN",
+                user_image: "https://cdn2.hubspot.net/hubfs/53/image8-2.jpg",
+              },
+              {
+                user_id: "memId_00000000000003",
+                user_name: "김이름2",
+                user_status: "INACTIVE",
+                user_image: "https://cdn2.hubspot.net/hubfs/53/image8-2.jpg",
+              },
+              {
+                user_id: "memId_00000000000004",
+                user_name: "김이름3",
+                user_status: "REMOVED",
+                user_image: "https://cdn2.hubspot.net/hubfs/53/image8-2.jpg",
+              },
+              {
+                user_id: "memId_00000000000005",
+                user_name: "김이름4",
+                user_status: "BANNED",
+                user_image: "https://cdn2.hubspot.net/hubfs/53/image8-2.jpg",
+              },
+            ]
+      );
+      setDomLoaded(true);
+    }
+  }, [code, response]);
+
+  useEffect(() => {
+    fetchData("get", "/api/userMng/getUserList", null, true);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const [productLinkInputText, setProductLinkInputText] = useState("");
@@ -74,30 +86,6 @@ const UserMngComp = () => {
     // fetchData("/api/admin/item/searchItem", data, true);
   };
 
-  /**
-   * 서버 선택 이벤트
-   */
-  const [isClicked, setIsClicked] = useState(false); // server selected YN
-  const selectBoxBtnHandler = () => {
-    setIsClicked(!isClicked);
-  };
-
-  const [trdServerList, setTrdServerList] = useState<any>(null); // 조회된 서버 리스트
-  const [selectedValue, setSelectedValue] = useState<any>(null); // 현재 선택된 서버
-  let selectedServerNm = trdServerList?.map((state: any, index: number) => (
-    <li key={index}>
-      <div className="selectbutton" onClick={() => answerStateValueSelectedHandler(state)}>
-        {state}
-      </div>
-    </li>
-  ));
-
-  const answerStateValueSelectedHandler = (state: string) => {
-    console.log(state);
-    setSelectedValue(state);
-    setIsClicked(!isClicked);
-  };
-
   const goUsrMng = (userId: String) => {
     router.push(`/userMng/userDtl/${userId}`);
   };
@@ -106,7 +94,7 @@ const UserMngComp = () => {
     <>
       <div className="mainComponent">
         <h1>회원관리</h1>
-        <div className="w-[700px] h-[800px] relative overflow-hidden bg-white">
+        <div className="w-[500px] h-[800px] relative overflow-hidden bg-white mx-auto my-0">
           <div className="w-full">
             <div className="srch_area">
               <div className="frm_srch">
@@ -137,7 +125,7 @@ const UserMngComp = () => {
               usrList.map((usr: any, index: number) => {
                 return (
                   <div key={String(usr.user_id)}>
-                    <div className="w-[500px] usrListGrp px-3 py-3">
+                    <div className="w-[400px] usrListGrp px-3 py-3 mx-auto my-0">
                       <div className="flex">
                         <div className="relative h-[50px] w-[50px] rounded-full overflow-hidden">
                           <Image src={usr.user_image} alt={"usrImg"} layout="fill"></Image>
@@ -146,15 +134,7 @@ const UserMngComp = () => {
                           {usr.user_name}
                         </div>
                       </div>
-                      <div className="select_box_small" id="select_box_small">
-                        <div className={`select_box_small ${isClicked && "open"}`} id="select_box_small">
-                          <button className="select_txt_small" id="select_txt_small" onClick={selectBoxBtnHandler}>
-                            {/* {selectedValue} */}
-                            {codeToKor(usr.user_status)}
-                          </button>
-                          {!!selectedServerNm && <ul className="select_lst">{selectedServerNm}</ul>}
-                        </div>
-                      </div>
+                      <div className="text-[#00c1a6] font-bold	py-3">{codeToKor(usr.user_status)}</div>
                     </div>
                   </div>
                 );
