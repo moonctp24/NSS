3465ad76116c71a4a12e54c2b72e9d58591db39d
moonctp <moonctp24@gmail.com>
2024-04-07 15:25:12 +0900

    의약품리스트 실제 통신 데이터로 변경

--- components/medicineMng/MedicineListTable.tsx
+++ components/medicineMng/MedicineListTable.tsx
@@ -1,4 +1,5 @@
 import { dateFormat } from "@/constants/util/commUtil";
+import { confirmAction } from "@/store/modal/confirm-slice";
 import axios from "axios";
 // import { confirmAction } from "@/store/modal/confirm-slice";
 import Image from "next/image";
@@ -7,28 +8,30 @@ import { useDispatch } from "react-redux";
 
 const MedicineListTable = (props: any) => {
   const medicineList = props.medicineList;
+  console.log(medicineList);
   const dispatch = useDispatch();
   const router = useRouter();
 
   const itemDelBtn = (itemNo: String) => {
     // alert(itemNo + " 를 삭제하시겠습니까?");
-    // dispatch(confirmAction.openModal({ cont: itemNo + " 를 삭제하시겠습니까?" }));
+    dispatch(confirmAction.openModal({ cont: itemNo + " 를 삭제하시겠습니까?" }));
     console.log(itemNo + " deleted");
-    const AXIOS = axios.create({
-      withCredentials: true,
-      headers: {
-        "Content-Type": "application/json; charset=utf-8",
-      },
-    });
+    // const AXIOS = axios.create({
+    //   withCredentials: true,
+    //   headers: {
+    //     "Content-Type": "application/json; charset=utf-8",
+    //   },
+    // });
     let data = {
       item_seq: itemNo,
     };
 
-    const res = AXIOS.post("http://3.39.214.33:8081/api/login", JSON.stringify(data));
-    console.log(res);
+    // const res = AXIOS.post("http://3.39.214.33:8081/api/login", JSON.stringify(data));
+    // console.log(res);
   };
 
   const goMdcnUpdPage = (itemNo: String) => {
+    console.log(itemNo);
     router.push(`/medicineMng/mdcnUpd/${itemNo}`);
   };
 
@@ -60,20 +63,20 @@ const MedicineListTable = (props: any) => {
             medicineList?.map((m: any, index: number) => {
               if ((props.nowTablePage - 1) * 5 <= index && index < (props.nowTablePage - 1) * 5 + 5) {
                 return (
-                  <tr key={String(m.item_seq)}>
+                  <tr key={String(m.itemSeq)}>
                     <td className="t_center">{index + 1}</td>
-                    <td className="t_left">{m.medicine_name}</td>
+                    <td className="t_left">{m.medicineName}</td>
                     <td className="t_center">
                       <Image width={50} height={50} src={m.pill_image} alt={"pillImg"}></Image>
                     </td>
-                    <td className="t_left">{m.company_name}</td>
+                    <td className="t_left">{m.companyName}</td>
                     <td className="t_center">{dateFormat(m.created_at)}</td>
                     <td className="t_center">{dateFormat(m.updated_at)}</td>
                     <td className="">
                       <button
                         className="inTableBtn"
                         onClick={() => {
-                          goMdcnUpdPage(m.item_seq);
+                          goMdcnUpdPage(m.itemSeq);
                         }}
                       >
                         수정
@@ -81,7 +84,7 @@ const MedicineListTable = (props: any) => {
                       <button
                         className="btn_gray inTableBtn"
                         onClick={(e: any) => {
-                          itemDelBtn(m.item_seq);
+                          itemDelBtn(m.itemSeq);
                         }}
                       >
                         삭제
--- components/medicineMng/MedicineMngComp.tsx
+++ components/medicineMng/MedicineMngComp.tsx
@@ -1,6 +1,7 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import MedicineListTable from "./MedicineListTable";
 import { useRouter } from "next/router";
+import { useAxios } from "@/constants/util/API_UTIL";
 
 const MedicineMngComp = () => {
   const [domLoaded, setDomLoaded] = useState(false); // 테이블 데이터 로딩 상태
@@ -8,96 +9,120 @@ const MedicineMngComp = () => {
   const [nowTablePage, setNowTablePage] = useState(1);
   const [ingredientCnt, setIngredientCnt] = useState(0);
 
+  const { code, response, fetchData } = useAxios();
+
   const router = useRouter();
 
+  useEffect(() => {
+    getResult();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [code, response, router]);
+
+  const getResult = useCallback(() => {
+    // console.log(`code :: ${code} / response :: `, response);
+    if (response && code == "200") {
+      console.log(response);
+      const tmpObject = response;
+      const tmpList: { [s: string]: any } = [];
+      // object 형으로 받아와서 배열 형태로 변환해주기
+      let i = 0;
+      for (const [key, value] of Object.entries(tmpObject)) {
+        tmpList[i] = value;
+        i++;
+      }
+      setMedicineList(tmpList);
+    }
+  }, [code, response]);
   const goAddMdcnPage = () => {
     router.push("/medicineMng/mdcnAdd");
   };
 
   useEffect(() => {
-    setMedicineList([
-      {
-        item_seq: "medicineId_99999999999995",
-        medicine_name: "약이름",
-        company_name: "약회사",
-        pill_image: "https://cdn2.hubspot.net/hubfs/53/image8-2.jpg",
-        created_at: "20240301153021",
-        updated_at: "20240301153021",
-      },
-      {
-        item_seq: "medicineId_99999999999994",
-        medicine_name: "약이름2",
-        company_name: "약회사2",
-        pill_image: "https://cdn2.hubspot.net/hubfs/53/image8-2.jpg",
-        created_at: "20240301153021",
-        updated_at: "20240301153021",
-      },
-      {
-        item_seq: "medicineId_99999999999993",
-        medicine_name: "약이름3",
-        company_name: "약회사3",
-        pill_image: "https://cdn2.hubspot.net/hubfs/53/image8-2.jpg",
-        created_at: "20240301153021",
-        updated_at: "20240301153021",
-      },
-      {
-        item_seq: "medicineId_99999999999991",
-        medicine_name: "약이름4",
-        company_name: "약회사4",
-        pill_image: "https://cdn2.hubspot.net/hubfs/53/image8-2.jpg",
-        created_at: "20240301153021",
-        updated_at: "20240301153021",
-      },
-      {
-        item_seq: "medicineId_99999999999992",
-        medicine_name: "약이름5",
-        company_name: "약회사5",
-        pill_image: "https://cdn2.hubspot.net/hubfs/53/image8-2.jpg",
-        created_at: "20240301153021",
-        updated_at: "20240301153021",
-      },
-      {
-        item_seq: "medicineId_99999999999980",
-        medicine_name: "약이름6",
-        company_name: "약회사",
-        pill_image: "https://cdn2.hubspot.net/hubfs/53/image8-2.jpg",
-        created_at: "20240301153021",
-        updated_at: "20240301153021",
-      },
-      {
-        item_seq: "medicineId_99999999999981",
-        medicine_name: "약이름7",
-        company_name: "약회사2",
-        pill_image: "https://cdn2.hubspot.net/hubfs/53/image8-2.jpg",
-        created_at: "20240301153021",
-        updated_at: "20240301153021",
-      },
-      {
-        item_seq: "medicineId_99999999999982",
-        medicine_name: "약이름8",
-        company_name: "약회사3",
-        pill_image: "https://cdn2.hubspot.net/hubfs/53/image8-2.jpg",
-        created_at: "20240301153021",
-        updated_at: "20240301153021",
-      },
-      {
-        item_seq: "medicineId_99999999999983",
-        medicine_name: "약이름9",
-        company_name: "약회사4",
-        pill_image: "https://cdn2.hubspot.net/hubfs/53/image8-2.jpg",
-        created_at: "20240301153021",
-        updated_at: "20240301153021",
-      },
-      {
-        item_seq: "medicineId_99999999999984",
-        medicine_name: "약이름10",
-        company_name: "약회사5",
-        pill_image: "https://cdn2.hubspot.net/hubfs/53/image8-2.jpg",
-        created_at: "20240301153021",
-        updated_at: "20240301153021",
-      },
-    ]);
+    fetchData("get", "/api/medicineMng/getMdcnList", null, true);
+    // setMedicineList([
+    //   {
+    //     item_seq: "medicineId_99999999999995",
+    //     medicine_name: "약이름",
+    //     company_name: "약회사",
+    //     pill_image: "https://cdn2.hubspot.net/hubfs/53/image8-2.jpg",
+    //     created_at: "20240301153021",
+    //     updated_at: "20240301153021",
+    //   },
+    //   {
+    //     item_seq: "medicineId_99999999999994",
+    //     medicine_name: "약이름2",
+    //     company_name: "약회사2",
+    //     pill_image: "https://cdn2.hubspot.net/hubfs/53/image8-2.jpg",
+    //     created_at: "20240301153021",
+    //     updated_at: "20240301153021",
+    //   },
+    //   {
+    //     item_seq: "medicineId_99999999999993",
+    //     medicine_name: "약이름3",
+    //     company_name: "약회사3",
+    //     pill_image: "https://cdn2.hubspot.net/hubfs/53/image8-2.jpg",
+    //     created_at: "20240301153021",
+    //     updated_at: "20240301153021",
+    //   },
+    //   {
+    //     item_seq: "medicineId_99999999999991",
+    //     medicine_name: "약이름4",
+    //     company_name: "약회사4",
+    //     pill_image: "https://cdn2.hubspot.net/hubfs/53/image8-2.jpg",
+    //     created_at: "20240301153021",
+    //     updated_at: "20240301153021",
+    //   },
+    //   {
+    //     item_seq: "medicineId_99999999999992",
+    //     medicine_name: "약이름5",
+    //     company_name: "약회사5",
+    //     pill_image: "https://cdn2.hubspot.net/hubfs/53/image8-2.jpg",
+    //     created_at: "20240301153021",
+    //     updated_at: "20240301153021",
+    //   },
+    //   {
+    //     item_seq: "medicineId_99999999999980",
+    //     medicine_name: "약이름6",
+    //     company_name: "약회사",
+    //     pill_image: "https://cdn2.hubspot.net/hubfs/53/image8-2.jpg",
+    //     created_at: "20240301153021",
+    //     updated_at: "20240301153021",
+    //   },
+    //   {
+    //     item_seq: "medicineId_99999999999981",
+    //     medicine_name: "약이름7",
+    //     company_name: "약회사2",
+    //     pill_image: "https://cdn2.hubspot.net/hubfs/53/image8-2.jpg",
+    //     created_at: "20240301153021",
+    //     updated_at: "20240301153021",
+    //   },
+    //   {
+    //     item_seq: "medicineId_99999999999982",
+    //     medicine_name: "약이름8",
+    //     company_name: "약회사3",
+    //     pill_image: "https://cdn2.hubspot.net/hubfs/53/image8-2.jpg",
+    //     created_at: "20240301153021",
+    //     updated_at: "20240301153021",
+    //   },
+    //   {
+    //     item_seq: "medicineId_99999999999983",
+    //     medicine_name: "약이름9",
+    //     company_name: "약회사4",
+    //     pill_image: "https://cdn2.hubspot.net/hubfs/53/image8-2.jpg",
+    //     created_at: "20240301153021",
+    //     updated_at: "20240301153021",
+    //   },
+    //   {
+    //     item_seq: "medicineId_99999999999984",
+    //     medicine_name: "약이름10",
+    //     company_name: "약회사5",
+    //     pill_image: "https://cdn2.hubspot.net/hubfs/53/image8-2.jpg",
+    //     created_at: "20240301153021",
+    //     updated_at: "20240301153021",
+    //   },
+    // ]);
     setDomLoaded(true);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
   useEffect(() => {
     setIngredientCnt(medicineList?.length);
