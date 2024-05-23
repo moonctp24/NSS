d9bbd73b576f9af350b6a7ac7ed52075d492601d
moonctp <moonctp24@gmail.com>
2024-05-24 08:46:16 +0900

    더미로 페이지 레이아웃 설정

--- components/layout/main/MainComp.tsx
+++ components/layout/main/MainComp.tsx
@@ -1,6 +1,5 @@
 import type { NextPage } from "next";
 import dynamic from "next/dynamic";
-// import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 
 const Chart = dynamic(
@@ -11,16 +10,13 @@ const Chart = dynamic(
 );
 
 const MainComp: NextPage = () => {
-  // const router = useRouter();
-
-  // const goUserMngBtn = () => {
-  //   router.push("/admin/userMng");
-  // };
-
   const [isLoad, setIsLoad] = useState(false);
+  const [initData, setInitData] = useState<any>();
+
   useEffect(() => {
     setTimeout(() => {
       setIsLoad(true);
+      setInitData([0, 19, 26, 20, 19, 30]);
     }, 1000);
   }, []);
 
@@ -29,19 +25,123 @@ const MainComp: NextPage = () => {
       <div className="mainComponent">
         <h1>대시보드</h1>
         {isLoad ? (
-          <Chart
-            type="line"
-            series={[
-              { name: "오늘의 기온", data: [19, 26, 20, 9] },
-              { name: "내일의 기온", data: [30, 26, 34, 10] },
-            ]}
-            options={{
-              chart: {
-                height: 100,
-                width: 100,
-              },
-            }}
-          />
+          <>
+            <div className="headerFlex">
+              <div className="w-full">
+                <Chart
+                  type="line"
+                  series={[{ name: "가입자 수 추이", data: initData }]}
+                  options={{
+                    chart: {
+                      height: 100,
+                      width: 100,
+                    },
+                  }}
+                />
+                <div className="text-center">가입자 수 추이</div>
+              </div>
+              <div className="w-full">
+                <Chart
+                  type="line"
+                  series={[
+                    { name: "사용률", data: [40, 60, 70, 75] },
+                    { name: "가입율", data: [30, 26, 34, 40] },
+                  ]}
+                  options={{
+                    chart: {
+                      height: 100,
+                      width: 100,
+                    },
+                  }}
+                />
+              </div>
+              <div className="w-full">
+                <Chart
+                  type="bar"
+                  series={[
+                    { name: "오늘의 기온", data: [19, 26, 20, 9] },
+                    { name: "내일의 기온", data: [30, 26, 34, 10] },
+                  ]}
+                  options={{
+                    chart: {
+                      height: 100,
+                      width: 100,
+                    },
+                  }}
+                />
+              </div>
+            </div>
+            <div className="headerFlex">
+              <div className="w-full">
+                <Chart
+                  type="line"
+                  series={[
+                    { name: "오늘의 기온", data: [19, 26, 20, 9] },
+                    { name: "내일의 기온", data: [30, 26, 34, 10] },
+                  ]}
+                  options={{
+                    theme: {
+                      mode: "dark",
+                    },
+                    chart: {
+                      height: 100,
+                      width: 100,
+                    },
+                  }}
+                />
+              </div>
+              <div className="w-full">
+                <Chart
+                  type="line"
+                  series={[
+                    { name: "오늘의 기온", data: [19, 26, 20, 9] },
+                    { name: "내일의 기온", data: [30, 26, 34, 10] },
+                  ]}
+                  options={{
+                    chart: {
+                      type: "candlestick",
+                      height: 350,
+                      width: 500,
+                      toolbar: {
+                        show: false,
+                      },
+                      background: "transparent",
+                    },
+                    stroke: {
+                      curve: "smooth",
+                      width: 2,
+                    },
+                    yaxis: {
+                      show: false,
+                    },
+                    plotOptions: {
+                      candlestick: {
+                        colors: {
+                          upward: "#3C90EB",
+                          downward: "#DF7D46",
+                        },
+                      },
+                    },
+                  }}
+                />
+              </div>
+              <div className="w-full">
+                <Chart
+                  type="line"
+                  series={[
+                    { name: "오늘의 기온", data: [19, 26, 20, 9] },
+                    { name: "내일의 기온", data: [30, 26, 34, 10] },
+                  ]}
+                  options={{
+                    chart: {
+                      height: 100,
+                      width: 100,
+                    },
+                  }}
+                />
+              </div>
+            </div>
+          </>
         ) : (
           <div>is loading</div>
         )}
