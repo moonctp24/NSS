1fdc61e811a1bedc2aabb53387dd60ee932c79dc
moonctp <moonctp24@gmail.com>
2024-07-30 12:23:47 +0900

    필요없는 코드 정리

--- constants/CONST.tsx
+++ constants/CONST.tsx
@@ -7,23 +7,3 @@ export const NAUTH_PAGE_LIST = ["/nauth/login", "/nauth/findpw", , "/devtest"];
  * 유효하지 않은 페이지
  */
 export const NON_VALIED = "/404";
-
-/**
- * LOGIN INFORMATION KEY CONSTANT
- */
-// export const COOKIES = {
-//   IS_LOGIN: "isLogin",
-//   ID: "id",
-//   EXPIRE_DATE: "expireDate",
-//   ACCESS_TOKEN: "token",
-//   REFRESH_TOKEN: "refreshToken",
-// };
-
-/**
- * FETCH CONTENT TYPE'S CONSTANT
- */
-// export const CONTENT_TYPE = {
-//   KEY: "Content-Type",
-//   JSON: "application/json",
-//   MULTI: "multipart/form-data",
-// };
